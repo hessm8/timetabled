0bb7fd8ea34218bca6425f37c75e77d1613a9bd3
Jack <jackshitspy@mail.ru>
2021-03-30 17:42:01 +0700

    GuiManager - More appropriate look of schedule, now displays all lessons instead of only 1

--- Timetabled/GuiManager.cs
+++ Timetabled/GuiManager.cs
@@ -15,7 +15,7 @@ namespace Timetabled {
         public void CreateSchedule() {
             for (int dayIndex = 0; dayIndex < 7; dayIndex++) {
                 var pos = new Point(dayIndex * offset.X + 30, 30);
-                var size = new Size(200, 600);
+                var size = new Size(192, 475);
                 var comboSize = new Size(150, 20);
 
                 var dayGroup = new GroupBox() {
@@ -28,27 +28,35 @@ namespace Timetabled {
                 for (int lessonIndex = 0; lessonIndex < 6; lessonIndex++) {
                     int lessonOffset = lessonIndex * 70;
 
+                    var lessonPanel = new Panel() {
+                        Location = new Point(20, pos.Y + lessonOffset),
+                        Size = new Size(152, 63),
+                        BorderStyle = BorderStyle.FixedSingle                        
+                    };
+
                     var subjectBox = new ComboBox() {
                         //Name = $"subjectBox{dayIndex + 1}",
-                        Location = new Point(20, pos.Y + lessonOffset),
+                        Location = new Point(0, 0),
                         Size = comboSize
                     };
 
                     var teacherBox = new ComboBox() {
                         //Name = $"teacherBox{dayIndex + 1}",
-                        Location = new Point(20, pos.Y + offset.Y + lessonOffset),
+                        Location = new Point(0, offset.Y),
                         Size = comboSize
                     };
 
                     var roomBox = new ComboBox() {
                         //Name = $"roomBox{dayIndex + 1}",
-                        Location = new Point(20, pos.Y + offset.Y * 2 + lessonOffset),
+                        Location = new Point(0, offset.Y * 2),
                         Size = comboSize
                     };
 
-                    dayGroup.Controls.AddRange(new Control[] {
+                    lessonPanel.Controls.AddRange(new Control[] {
                         subjectBox, teacherBox, roomBox
                     });
+
+                    dayGroup.Controls.Add(lessonPanel);
                 }
 
                 controls.Add(dayGroup);
